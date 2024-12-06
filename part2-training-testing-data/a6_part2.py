4b513860bbc793abbbf787e5fd726518a330e9a6
Jovial-10 <jbrana@cps.edu>
2024-12-06 11:04:30 -0600

    xtrain

--- part2-training-testing-data/a6_part2.py
+++ part2-training-testing-data/a6_part2.py
@@ -21,6 +21,7 @@ xtrain, xtest, ytrain, ytest = train_test_split(x,y,test_size = .2)
 # Use reshape to turn the x values into 2D arrays:
 
 x = x.reshape(-1,1)
+xtrain=xtrain.reshape(-1,1)
 
 # Create the model
 
